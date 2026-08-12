94a02390b1c3d5077aadccac94549b6069ca42dd
Piotr <piaskowski1993@users.noreply.github.com>
2026-08-12 12:03:33 +0200

    Wire up Thread + Join orchestration in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading;
+using drone;
+using drone.ThreadRace;
+
+var alpha = new DroneModel ("Alpha", maxCheckpoints : 5, delayMs: 300);
+var bravo = new DroneModel ("Bravo", maxCheckpoints :5, delayMs: 300);
+
+var threadAlpha = new Thread (() => DroneThreadWorker.Fly(alpha));
+var threadBravo = new Thread (() => DroneThreadWorker.Fly(bravo));
+
+threadAlpha.Start();
+threadBravo.Start();
+
+threadAlpha.Join();
+threadBravo.Join();
+Console.WriteLine("All drones finished.");
